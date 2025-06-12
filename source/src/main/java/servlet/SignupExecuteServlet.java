5a2a7911bb1233ec572233c688602a58a0800087
plusdojo2025-yukookada <okada-yuko-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 17:50:38 +0900

    新規登録時、マスタータスクをタスクテーブルに複製処理

--- source/src/main/java/servlet/SignupExecuteServlet.java
+++ source/src/main/java/servlet/SignupExecuteServlet.java
@@ -1,6 +1,9 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,7 +14,11 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import dao.AccountDAO;
+import dao.MasterTaskDAO;
+import dao.TasksDAO;
 import dto.Account;
+import dto.MasterTask;
+import dto.Task;
 
 /**
  * アカウントの新規登録の実行処理をするサーブレット
@@ -35,7 +42,40 @@ public class SignupExecuteServlet extends HttpServlet {
 		boolean result = false;
 
 	    try {
+	    	// アカウント情報をDBに登録
 	    	result = accountDAO.registAccount(signupAccount);
+	    	
+			if (result) {
+				// マスタータスクを取得
+				MasterTaskDAO masterTaskDAO = new MasterTaskDAO();
+				List<MasterTask> masterTasks = masterTaskDAO.selectAll();
+
+				// タスクDAOを使ってタスクをアカウント用に登録
+				TasksDAO taskDAO = new TasksDAO();
+				for (MasterTask masterTask : masterTasks) {
+					// 複製用のタスクインスタンスを作成
+					Task userTask = new Task();
+					userTask.setAccountId(signupAccount.getAccountId());
+					userTask.setTitle(masterTask.getTitle());
+					userTask.setTimeSpan(masterTask.getTimeSpan());
+					userTask.setMoodId(masterTask.getMoodId());
+					userTask.setCategoryId(masterTask.getCategoryId());
+					userTask.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
+					userTask.setIsPrivate(false);
+
+					// 登録処理
+					taskDAO.registTask(userTask);
+				}
+				
+				// 登録成功：セッション情報の削除・LoginServletへリダイレクト
+				session.removeAttribute("signup_user");
+				response.sendRedirect("LoginServlet");
+			} else {
+				// 登録失敗:エラー画面
+				request.setAttribute("errorMessage", "登録に失敗しました。もう一度お試しください。");
+				RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/error.jsp");
+				dispatcher.forward(request, response);
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
             request.setAttribute("errorMessage", "アカウント情報の登録中にエラーが発生しました。");
@@ -44,16 +84,7 @@ public class SignupExecuteServlet extends HttpServlet {
             return;
 		}
 	    
-		if (result) {
-			// 登録成功：セッション情報の削除・LoginServletへリダイレクト
-			session.removeAttribute("signup_user");
-			response.sendRedirect("LoginServlet");
-		} else {
-			// 登録失敗:エラー画面
-			request.setAttribute("errorMessage", "登録に失敗しました。もう一度お試しください。");
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/error.jsp");
-			dispatcher.forward(request, response);
-		}
+
 	}
 
 }
