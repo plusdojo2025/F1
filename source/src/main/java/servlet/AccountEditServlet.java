343caf172254cce644b948ec2cdbda9ebf968359
plusdojo2025-mandai <mandai-fumina-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 16:50:51 +0900

    アカウント関連のサーブレット作成

--- source/src/main/java/servlet/AccountEditServlet.java
+++ source/src/main/java/servlet/AccountEditServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dto.Account;
+
 
 /**
  * Servlet implementation class AccountEditServlet
@@ -19,15 +25,25 @@ public class AccountEditServlet extends HttpServlet {
      */
     public AccountEditServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		// リクエストパラメータを取得する
+		request.setCharacterEncoding("UTF-8");
+		int flag =  Integer.parseInt(request.getParameter("flag"));
+		
+		if(flag == 1) {
+			HttpSession session = request.getSession();
+			Account account = (Account)session.getAttribute("login_user");	
+			session.setAttribute("account", account);
+		}
+		//アカウント情報変更画面へフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/account_edit.jsp");
+		dispatcher.forward(request, response);
+		
 	}
 
 	/**
--- source/src/main/java/servlet/AccountUpdateServlet.java
+++ source/src/main/java/servlet/AccountUpdateServlet.java
@@ -1,11 +1,17 @@
 package servlet;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.AccountDAO;
+import dto.Account;
 
 /**
  * Servlet implementation class AccountUpdateServlet
@@ -19,23 +25,44 @@ public class AccountUpdateServlet extends HttpServlet {
      */
     public AccountUpdateServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		//リクエストパラメーターを取得する
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
+		
+		try {
+			
+
+		//更新を行う
+		AccountDAO aDao = new AccountDAO();
+		Account account = (Account)session.getAttribute("account");
+		if(aDao.updateAccount(account)) {
+			// 更新成功
+			
+			session.setAttribute("login_user", account);
+			session.removeAttribute("account");
+			response.sendRedirect("AccountServlet");
+		}
+		
+		}catch (Exception e) {
+            // DB例外・その他予期しないエラー時
+            request.setAttribute("loginErrorMassage", "ユーザー情報の変更中にエラーが発生しました");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/login.jsp");
+            dispatcher.forward(request, response);
+        }
+		
 	}
 
 }
