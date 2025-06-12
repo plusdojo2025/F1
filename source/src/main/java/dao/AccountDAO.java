fe585948d1f6b4aa2d58375c97ae6196c9c23d8d
plusdojo2025-Numasawa <numasawa-shion-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-12 10:20:59 +0900

    daoの作成

---
+++ source/src/main/java/dao/AccountDAO.java
@@ -0,0 +1,26 @@
+package dao;
+
+import bean.Account;
+		
+public class AccountDAO {
+	
+
+	// 1件のアカウントを取得
+	public Account getAccount(String email, String password) {
+			
+	}
+		
+	// 1件のアカウントを新規登録
+	public boolean registAccount(Account account) {
+	// 重複があった場合：return false;
+			
+	}
+		
+	// アカウント情報の更新
+	public boolean updateAccount(Account account) {
+			
+	}
+	
+
+
+}
