f5f00b11acd6eea312aafcaecceed423883ae0a0
plusdojo2025-yukookada <okada-yuko-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 16:36:32 +0900

    マージ処理その３

--- source/src/main/java/dao/MoodDAO.java
+++ source/src/main/java/dao/MoodDAO.java
@@ -21,6 +21,7 @@ public class MoodDAO {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sukima?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
 					"root", "password");
 		
@@ -59,6 +60,7 @@ public class MoodDAO {
 	}
 	
 	// Moodテーブルから全件取得
+	public List<Mood> getMoodList() {
 		
 		Connection conn = null;
 		// リストを作成
@@ -67,6 +69,7 @@ public class MoodDAO {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			
+			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/sukima?"
 					+ "characterEncoding=utf8&useSSL=false&serverTimezone=GMT%2B9&rewriteBatchedStatements=true",
 					"root", "password");
 		
@@ -86,7 +89,25 @@ public class MoodDAO {
 			}
 
 			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			list = null;
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+			list = null;
+		} finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+					list = null;
+				}
+			}
+		}
 		
+	return list;
 		
 	}
 
