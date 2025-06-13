564378e85e1d144e6620e5d5b02e13eda642e867
plusdojo2025-yukookada <okada-yuko-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-13 10:59:16 +0900

    SuggestServletの作成

--- source/src/main/java/servlet/SuggestServlet.java
+++ source/src/main/java/servlet/SuggestServlet.java
@@ -1,41 +1,77 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.TasksDAO;
+import dto.Account;
+import dto.Task;
 
 /**
- * Servlet implementation class SuggestServlet
+ * 入力されたデータを基に自身のタスクリスト・他の人のタスクリストを取得。
+ * 提案実行画面へリダイレクト。
  */
 @WebServlet("/SuggestServlet")
 public class SuggestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SuggestServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	 /**
+	  * POSTリクエスト時の処理
+	  */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	    // 文字コード設定
+	    request.setCharacterEncoding("UTF-8");
+	    response.setContentType("text/html; charset=UTF-8");
+	    
+	    // セッション取得・アカウント情報の取得
+	    HttpSession session = request.getSession();
+	    Account loginAccount = (Account) session.getAttribute("login_user");
+	     
+	    // セッションにアカウント情報が無い場合
+	    if (loginAccount == null) {
+	   	    response.sendRedirect("LoginServlet");
+	   	    return;
+	    }
+	     
+	    // リクエストパラメーターの取得
+	    int spanTime = Integer.parseInt(request.getParameter("spanTime"));
+	    int moodId = Integer.parseInt(request.getParameter("moodId"));
+	    int categoryId = Integer.parseInt(request.getParameter("categoryId"));
+	     
+        // リストの生成
+        List<Task> suggestTaskList = new ArrayList<>();
+        List<Task> suggestOtherTaskList = new ArrayList<>();
+	     
+		// DAOの生成
+		TasksDAO tasksDAO = new TasksDAO();
+		
+		try {
+			suggestTaskList = tasksDAO.suggestTask(loginAccount.getAccountId(), spanTime, moodId, categoryId);
+			suggestOtherTaskList = tasksDAO.suggestOtherTask(loginAccount.getAccountId(), spanTime, moodId, categoryId);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+            request.setAttribute("errorMessage", "エラーが発生しました。");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/error.jsp");
+            dispatcher.forward(request, response);
+            return;
+		}
+		
+	    // リクエストスコープにセット
+	    request.setAttribute("suggestTaskList", suggestTaskList);
+	    request.setAttribute("suggestOtherTaskList", suggestOtherTaskList);
+
+	    // 提案実行画面へフォワード
+        request.getRequestDispatcher("/WEB-INF/jsp/suggest.jsp").forward(request, response);
 	}
 
 }
