0d461643887ee366ad1dbf86db37d0c2f3e45c59
plusdojo2025-mandai <mandai-fumina-plusdojo2025@seplus2016.onmicrosoft.com>
2025-06-16 15:12:04 +0900

    alert.jsの修正、実績画面の修正

--- source/src/main/webapp/js/alert.js
+++ source/src/main/webapp/js/alert.js
@@ -1,3 +1,40 @@
 /**
  * 
  */
+ 
+ 'use strict'
+
+function submitClick(event){    
+    const formId = event.target.id;
+    if(formId === 'regist'){
+         const privateCheckbox = document.getElementById('isPrivate');
+	     if(privateCheckbox.checked){
+	         if (!window.confirm('タスクをこの内容で登録しますか？\nこのタスクは他のユーザーに公開されます。')) {
+	  	         event.preventDefault();
+	  	         return false;
+ 		      }
+	     }else {
+             if (!window.confirm('タスクをこの内容で登録しますか？')) {
+   	            event.preventDefault();
+                return false;
+             }
+         }
+        }
+    else{
+
+ const privateCheckbox = document.getElementById('isPrivate');
+	 if(privateCheckbox.checked){
+	  if (!window.confirm('タスクをこの内容に変更しますか？\nこのタスクは他のユーザーに公開されます。')) {
+	  	event.preventDefault();
+	  	return false;
+ 		}
+	}else {
+  if (!window.confirm('タスクをこの内容に変更しますか？')) {
+   	event.preventDefault();
+    return false;
+    
+  }
+  }
+}
+  
+}
